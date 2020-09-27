f8432b50dc4721d5c8ed894cd963a4e4e5dbfd39
tdeleon <compuaserv@gmail.com>
2020-09-27 15:58:56 -0600

    Core proyecto lumen

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+//Envio de parametro
+$router->get('user', function () use ($router) {
+    return "HOLA MUNDO";
+
+});
+
+//Recibe parametro
+$router->get('user/{id}', function ($id) {
+    return 'User '.$id;
+});
+
+$router->post('user123', function () {
+    return 'Primer prueba de json';
+});
+
+$router->get('home', function () {
+    return 'HOME';
+});
