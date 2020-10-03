7826840ac905ca367ba2d09471ee367068929e32
tdeleon <compuaserv@gmail.com>
2020-10-03 08:13:49 -0600

    RUTAS

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,32 @@ $router->post('/register','UserController@register');
 //$router->post('/author','AuthorController@index');
 //MUESTRA LOS AUTORES
 $router->post('/authors','AuthorController@showAllAuthors');
+$router->get('authors/{id}','AuthorController@showOneAuthor');
+$router->post('authors','AuthorController@create');
+$router->delete('authors/{id}','AuthorController@delete');
+$router->put('authors/{id}','AuthorController@update');
+
 //$router->get('authors',  ['uses' => 'AuthorController@showAllAuthors']);
 //$router->post('/register','UserController@register');
 //$router->post('/user/{id}',['middleware' => 'auth', 'user' => 'UserController@getUser']);
+
+//LIBROS
+$router->post('/Book/all','BookController@showAllBook');
+$router->get('Book/{id}','BookController@showOneBook');
+$router->post('Book','BookController@create');
+$router->delete('Book/{id}','BookController@delete');
+$router->put('Book/{id}','BookController@update');
+
+//CATEGORIAS
+$router->post('/Category/all','CategoryController@showAllBook');
+$router->get('Category/{id}','CategoryController@showOneBook');
+$router->post('Category','CategoryController@create');
+$router->delete('Category/{id}','CategoryController@delete');
+$router->put('Category/{id}','CategoryController@update');
+
+//CATEGORIAS
+$router->post('/book_issued_detail/all','BookIssuedDetailsController@showAllBook');
+$router->get('book_issued_detail/{id}','BookIssuedDetailsController@showOneBook');
+$router->post('book_issued_detail','BookIssuedDetailsController@create');
+$router->delete('book_issued_detail/{id}','BookIssuedDetailsController@delete');
+$router->put('book_issued_detail/{id}','BookIssuedDetailsController@update');
