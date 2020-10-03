1c192e44dd97c5ac18d0964048010168be75a833
tdeleon <compuaserv@gmail.com>
2020-10-03 08:13:15 -0600

    CORRECCON AUTOR

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -13,4 +13,32 @@ class AuthorController extends Controller
       $res['message'] = 'Tu email es incorrecto 2';
       return response($res);*/
   }
+
+  public function showOneAuthor($id)
+   {
+       return response()->json(Autor::find($id));
+   }
+
+   public function create(Request $request)
+    {
+        $author = Autor::create($request->all());
+
+        return response()->json($author, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $author = Autor::findOrFail($id);
+        $author->update($request->all());
+
+        return response()->json($author, 200);
+    }
+
+    public function delete($id)
+    {
+        Autor::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
+    }
+
+
 }
